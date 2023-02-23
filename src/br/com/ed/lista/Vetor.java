c5b1cf35b2bb21c0a6cc669e7eab7d9fa2d2c49e
gitcleber <cleber.san@yahoo.com>
2023-02-23 14:37:43 -0300

    refactoring toString for uses StringBuilder class

--- src/br/com/ed/lista/Vetor.java
+++ src/br/com/ed/lista/Vetor.java
@@ -46,7 +46,24 @@ public class Vetor {
     }
 
     public String toString() {
-        return Arrays.toString(alunos);
+        // return Arrays.toString(alunos);
+        // refactoring: using Stringbuilder for show the list elements
+
+        if (this.totalDeAlunos == 0) {
+            return "[]";
+        }
+
+        StringBuilder builderString = new StringBuilder();
+        builderString.append("[");
+
+        for (int i = 0; i < this.totalDeAlunos - 1; i++) {
+            builderString.append(this.alunos[i]);
+            builderString.append(", ");
+        }
+        builderString.append(this.alunos[this.totalDeAlunos - 1]);
+        builderString.append("]");
+
+        return builderString.toString();
     }
 
 }
